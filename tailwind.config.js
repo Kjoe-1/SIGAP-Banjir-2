013b44368337497bdfe56657645125c4247b05ef
Binbang <bintang.arya.mahendr@gmail.com>
2026-06-25 17:29:17 +0700

    fix railway: ganti tailwind CDN ke lokal, bundle custom styles di dashboard.css

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/container-queries")],
+};
